4ab6ee166ede1dcb69047d416b4347b52b740bd3
xTiiX <anordmann.pro@gmail.com>
2022-09-12 10:06:23 +0200

    ADD : Restart project with Breeze / Back Login + Register done -> Need to do Front

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,11 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
         'email',
         'password',
+        'first_name',
+        'last_name',
+        'access_level_id',
     ];
 
     /**
@@ -31,6 +33,7 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'access_level_id',
     ];
 
     /**
